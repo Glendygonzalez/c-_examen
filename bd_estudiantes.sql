346505c3ffcf7d130bed0bcbe9e479f9ea2939a6
Glendygonzalez <81137515+Glendygonzalez@users.noreply.github.com>
2022-05-07 14:28:35 -0600

    Add files via upload

---
+++ bd_estudiantes.sql
@@ -0,0 +1,5 @@
+select * from estudiantes;
+insert into estudiantes (id_estudiante,carnet,nombres,apellidos,direccion,telefono,email,fecha_nacimiento)
+values (120,'123-7','glendy','gonzalez','Guatemala',5555,'gonzalez@gmail.com',current_timestamp());
+insert into estudiantes (carnet,nombres,apellidos,direccion,telefono,email,fecha_nacimiento)
+values ('20021','maribel','guite','Guatemala',8976,'maribelz@gmail.com',current_date());
